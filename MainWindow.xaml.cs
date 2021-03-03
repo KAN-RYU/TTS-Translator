8a624c76d9f7c3c71e16111711fa3d0c55831ba4
한준구 <kanryu2048@unist.ac.kr>
2021-03-03 23:07:08 +0900

    Complete UI Layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,25 @@ namespace TTS_Translator
         public MainWindow()
         {
             InitializeComponent();
+            TB_JSON_path.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\My Games\Tabletop Simulator\Saves";
+            TB_mod_folder_path.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\My Games\Tabletop Simulator\Mods";
+        }
+
+        private void Button_JSON_open_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".json";
+            dlg.Filter = "JSON (*.json)|*.json";
+            dlg.InitialDirectory = TB_JSON_path.Text;
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                TB_JSON_path.Text = filename;
+            }
         }
     }
 }
